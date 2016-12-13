1f4a05cc7ce894776ae161ce499e933e960db2f9
KirillStudent <kirill.megagroup+github@gmail.com>
2016-12-14 00:38:29 +0300

    Hello world

--- Unit1/src/HelloWorld.java
+++ Unit1/src/HelloWorld.java
@@ -1,8 +1,7 @@
-
 public class HelloWorld {
+
     public static void main(String[] args) {
-        int i = 3;
-        i = -i++ + i++ + -i;
-        System.out.println(i);
+
+
     }
 }
---
+++ Unit1/src/Second.java
@@ -0,0 +1,34 @@
+public class Second {
+
+//            int[] arrOrig = {7, 2, 9, 3, 10, 5, 1, 4, 6, 8};
+//            int[] arrRes = new int[arrOrig.length];
+//
+//            int e = 10;
+//
+//            for (int i = 0; i < arrOrig.length; i++) {
+//
+//                if (1 / Math.pow((arrOrig[i] + 1), 2) < e) {
+//
+//                    arrRes[i] = arrOrig[i]; //записываем только те элементы, которые удовлетворяют условию
+//                    System.out.println(arrRes[i]);
+//                }
+//            }
+//
+//            int min = arrRes[0];
+//
+//            for (int i = 0; i < arrRes.length; i++) { //?
+//                if (min > arrRes[i]) {
+//                    int tmp = min;
+//                    min = arrRes[i];
+//                    arrRes[--i] = tmp;
+//                }
+//
+//            }
+//
+//            for (int i = 0; i < arrRes.length; i++) {
+//                if (arrRes[i] == min) {
+//                    System.out.println("Номер минимального элемента со значением (" + min + "): " + i);
+//                    break;
+//                } //else System.out.println(i + " Что-то пошло не так");
+//            }
+}
